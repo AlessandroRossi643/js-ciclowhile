812f31d3da72f5db6b7e212da0a1afd418015c34
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-04-23 17:00:07 +0200

    fatta struttura es con for e while

---
+++ script.js
@@ -0,0 +1,23 @@
+// Creazione array cognomi
+var lista_cognomi=["Chiellini","Bonucci","Rugani","Bernardeschi","Zaniolo","Chiesa","Petagna","Gravina"];
+
+// Inserimento cognome utente tramite prompt e push nell'array
+var cognomeutente=prompt("Inserisci il tuo cognome!");
+lista_cognomi.push(cognomeutente);
+
+// Stampa lista ordinata alfabeticamente
+document.writeln(lista_cognomi.sort());
+
+// Stampa posizione cognome utente
+var i=0;
+while (i<lista_cognomi.length) {
+  if (lista_cognomi[i]==cognomeutente) {
+    console.log(i);
+  }
+  i++;
+}
+// for (var i = 0; i < lista_cognomi.length; i++) {
+//   if (lista_cognomi[i]==cognomeutente) {
+//     console.log(i);
+//   }
+// }
